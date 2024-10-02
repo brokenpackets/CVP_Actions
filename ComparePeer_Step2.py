588687e0a237a70ab296494e81f9bbd85e4cac36
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2024-10-02 07:40:08 -0700

    WIP

--- ComparePeer_Step2.py
+++ ComparePeer_Step2.py
@@ -4,21 +4,62 @@
 import re
 from cloudvision.cvlib import ActionFailed
 
-cmds = ["enable", "show mlag", "show lldp neighbors", "show ip bgp neighbors", "show interfaces status"]
+api_token = ''
+server1 = 'https://www.cv-prod-na-northeast1-b.arista.io'
+
+### Shim to work on 2023.1.3
+
+#!/usr/bin/env python
+import requests
+import json
+
+requests.packages.urllib3.disable_warnings()
+session = requests.Session()
+session.headers['Authorization'] = f"Bearer {api_token}"
+def get_configlet_by_name(url_prefix,configlet_name):
+  response = session.get(url_prefix+'/cvpservice/configlet/getConfigletByName.do?name='+configlet_name)
+  return response.json()
+####
+
+cmds = ["enable", "show mlag detail", "show lldp neighbors detail", "show ip bgp neighbors vrf all", "show interfaces status", "show hostname"]
 commands = ctx.runDeviceCmds(cmds, fmt="json")
+### MLAG
 mlagRaw = commands[1]["response"]
+peerMac = 'xxxxxxxx'
+mlagState = 'null'
 try:
-    mlagDomain = mlagRaw["domainId"]
+  mlagState = mlagRaw["negStatus"]
+  if mlagState == 'connected':
+    peerMac = mlagRaw['detail']['peerMacAddress'].replace(':','')
 except:
-    mlagDomain = 'spineTemp'
-mlagState = mlagRaw["state"]
+  pass
+hostname = commands[5]["response"]["hostname"]
+### LLDP
 lldpRaw = commands[2]["response"]["lldpNeighbors"]
 lldpPairs = []
+peerHostname = 'xxxxxxxx'
 for intf in lldpRaw:
-    lldpPeer = intf["neighborDevice"]
-    lldpPairs.append(lldpPeer)
+  if intf.startswith('Ethernet'):
+    lldpPeer = lldpRaw[intf]["lldpNeighborInfo"]
+    if lldpPeer:
+      try:
+        lldpPeername = lldpRaw[intf]["lldpNeighborInfo"][0]['systemName']
+        if lldpRaw[intf]["lldpNeighborInfo"][0]["chassisId"].replace('.','') == peerMac:
+          peerHostname = lldpRaw[intf]["lldpNeighborInfo"][0]["systemName"]
+        if lldpPeername.startswith(hostname[0:6]):
+          if lldpPeername.startswith(peerHostname):
+            pass
+          else:
+            lldpPairs.append(lldpPeername)
+      except:
+        pass
+if peerHostname == 'xxxxxxxx':
+  mlagPeer = 'spineTemp'
+else:
+  mlagPeer = peerHostname
 intfStatusRaw = commands[4]["response"]["interfaceStatuses"]
 intfParsed = []
+### Intf Status
 for interface in intfStatusRaw:
     multiLaneIntf = r'Ethernet[0-9]{1}\/|Ethernet1[0-9]{1}\/|Ethernet20\/'
     singleLaneIntf = r'Ethernet[0-9]{1}$|Ethernet1[0-9]{1}$|Ethernet20$'
@@ -28,35 +69,52 @@ for interface in intfStatusRaw:
     elif re.search(singleLaneIntf,interface):
       if intfStatusRaw[interface]["linkStatus"] == 'connected':
         intfParsed.append(interface)
-bgpRaw = commands[3]["response"]["vrfs"]["default"]["peerList"]
-bgpRouterIDs = []
-bgpLocalRouterID = '0.0.0.0'
-for peer in bgpRaw:
-    if peer["routerId"] == "0.0.0.0":
-      pass
-    else:
-      bgpRouterIDs.append(peer["routerId"])
-    if bgpLocalRouterID == '0.0.0.0':
-      bgpLocalRouterID = peer["localRouterId"]
-    elif bgpLocalRouterID == peer["localRouterId"]:
-      pass
-if bgpLocalRouterID != '0.0.0.0':
-  if mlagDomain != 'spineTemp':
-    bgpRouterIDs.append(bgpLocalRouterID)
-result = {"mlag": mlagState, "lldp": lldpPairs, "bgp": bgpRouterIDs, "intfStatus": intfParsed}
-PeerResult = ctx.retrieve(path=['compliance'],customKey=mlagDomain)
-#PeerResult = ctx.retrieve(path=['compliance'],customKey=mlagDomain,delete=False)
+### BGP
+bgpRaw = commands[3]["response"]["vrfs"]
+vrfRouterIDs = []
+pfxAccepted = []
+for vrf in bgpRaw:
+  vrfName = vrf
+  peerList = bgpRaw[vrf]['peerList']
+  bgpRouterIDs = []
+  bgpLocalRouterID = '0.0.0.0'
+  for peer in peerList:
+      if peer["routerId"] == "0.0.0.0":
+        pass
+      elif peer["localAsn"] == peer['asn']:
+        pass
+      else:
+        pfxReceived = peer['prefixesReceived']
+        bgpRouterIDs.append({peer["routerId"]: pfxReceived})
+      #if bgpLocalRouterID == '0.0.0.0':
+      #  bgpLocalRouterID = peer["localRouterId"]
+      #elif bgpLocalRouterID == peer["localRouterId"]:
+      #  pass
+  #if bgpLocalRouterID != '0.0.0.0':
+    #if mlagPeer != 'spineTemp':
+    #  bgpRouterIDs.append({bgpLocalRouterID: "0"})
+  vrfRouterIDs.append({"vrf": vrf, "routerIDs": bgpRouterIDs})
+## Collect results
+selfResult = {"mlag": mlagState, "selfHostname": hostname, "peerHostname": mlagPeer, "lldp": lldpPairs, "bgp": vrfRouterIDs, "intfStatus": intfParsed}
+#PeerResult = ctx.retrieve(path=['compliance'],customKey=hostname)
+#PeerResult = ctx.retrieve(path=['compliance'],customKey=hostname, delete=False)
+PeerResult = json.loads(get_configlet_by_name(server1,hostname)['config'])
+ctx.info(type(PeerResult))
+"""
 if not PeerResult:
-  raise ActionFailed(f"Peer result for {mlagDomain} not found")
-lldpCheck = set(result['lldp']) == set(PeerResult['lldp'])
+  raise ActionFailed(f"Peer result for {mlagPeer} not found")
+
+lldpCheck = set(selfResult['lldp']) == set(PeerResult['lldp'])
 if not lldpCheck:
   raise ActionFailed(f'lldp check failed.\nPeer: {PeerResult['lldp']}\nSelf: {result['lldp']}')
-bgpCheck = set(result['bgp']) == set(PeerResult['bgp'])
-if not bgpCheck:
-  raise ActionFailed(f'bgp check failed.\nPeer: {PeerResult['bgp']}\nSelf: {result['bgp']}')
-intfCheck = set(result['intfStatus']) == set(PeerResult['intfStatus'])
+if str(selfResult['bgp']) == str(PeerResult['bgp']):
+  ctx.info('True')
+#else:
+#  raise ActionFailed(f'bgp check failed.\nPeer: {PeerResult['bgp']}\nSelf: {result['bgp']}')
+intfCheck = set(selfResult['intfStatus']) == set(PeerResult['intfStatus'])
 if not intfCheck:
   raise ActionFailed(f'interface check failed.\nPeer: {PeerResult['intfStatus']}\nSelf: {result['intfStatus']}')
 
 #ctx.info(f"{PeerResult}")
 ctx.info(f"{'All checks passed.'}")
+"""
